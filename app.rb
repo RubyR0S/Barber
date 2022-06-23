98fb838b9578045eeea4d74a257c53ff19efc4a6
RubyR0S <fedorovych22@ukr.net>
2022-06-23 22:15:09 +0300

    First Commit

---
+++ app.rb
@@ -0,0 +1,14 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :batabase, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+end
+
+get '/' do
+	erb "Hello!"			
+end 
