d9318c9f05c86df3480967e36590e7bf64f27f3e
RubyR0S <fedorovych22@ukr.net>
2022-06-26 23:14:03 +0300

    Save clients information

--- app.rb
+++ app.rb
@@ -12,6 +12,33 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do 
+	@barbers = Barber.all
+end
+
 get '/' do 
+	
 	erb :index		
 end   
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	c = Client.new
+	c.name = @username
+	c.phone = @phone
+	c.datestamp = @datetime
+	c.barber = @barber
+	c.color = @color
+	c.save
+
+	erb "<h2>You singed up. See you!</h2>"
+end
