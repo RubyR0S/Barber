f1dbff4cfdb8fbf9ea2faaf2e0d8b3fe8faf2ca4
RubyR0S <fedorovych22@ukr.net>
2022-06-27 12:37:31 +0300

    Display error on failed validation

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ require 'sinatra/activerecord'
 set :database, {:adapter =>'sqlite3', :database=>'barbershop.db'}
 
 class Client < ActiveRecord::Base
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
@@ -28,7 +32,9 @@ end
 post '/visit' do
 
 	c = Client.new params[:client]
-	c.save
-
-	erb "<h2>You singed up. See you!</h2>"
+	if c.save
+        erb "<h2>You singed up. See you!</h2>"
+	else
+		erb "<h2>Error!</h2>"
+	end
 end
